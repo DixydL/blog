5aef7963c856ccc7f6ae4d19fa25b05627ce2b45
diexeed@gmail.com <diexeed@gmail.com>
2020-06-03 23:33:18 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+        <meta charset="utf-8" />
+        <title>Цвіріньк</title>
+
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+</head>
 <body>
 <div id="mute"></div>
 <div id="app"></div>
