2b81c338273e62e5ea1ae20cb279b04fea23de54
diexeed@gmail.com <diexeed@gmail.com>
2020-06-22 21:30:22 +0300

    fix

--- app/Helper/Symbol.php
+++ app/Helper/Symbol.php
@@ -7,7 +7,7 @@ class Symbol
     public static function chapterSymbolCount(int $symbol) : string
     {
         if ($symbol >= 1000) {
-            $symbol = (int)($symbol/1000);
+            $symbol = number_format(($symbol/1000), 1);
             $symbol = $symbol . "К";
         }
         return (string)$symbol;
