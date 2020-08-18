2fa7e2eebe9c6c741b4f5181c7aa6b84de9c2771
Dixyd <diexeed@gmail.com>
2020-08-18 23:16:48 +0300

    fix

--- app/Http/Controllers/API/V1/Tool/SubConverterController.php
+++ app/Http/Controllers/API/V1/Tool/SubConverterController.php
@@ -5,16 +5,26 @@ namespace App\Http\Controllers\API\V1\Tool;
 use ChaosTangent\ASS\Script;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use phpDocumentor\Reflection\DocBlock\Tags\Throws;
 
 class SubConverterController
 {
     public function index(Request $request)
     {
-        $plainTextData = "";
-        $subs = explode("\n", $request->text);
-        foreach ($subs as $sub) {
-            $value = explode(",,", $sub);
-            $plainTextData.="$value[1]\n\n";
+        if (empty($request->text)) {
+            return response("please fill sub ass");
+        }
+        try {
+            $plainTextData = "";
+            $subs = explode("\n", $request->text);
+            foreach ($subs as $sub) {
+                $value = explode(",,", $sub);
+                $plainText = str_replace("\N", "\n", $value[1]);
+                $plainText = preg_replace("/\{.*?\}|/", "", $plainText);
+                $plainTextData.="$plainText\n\n";
+            }
+        } catch (Throws $error) {
+            return response("Перевірте субтитри");
         }
 
         return response($plainTextData);
